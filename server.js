c838d36a52d5900d5819816fa0bc3fc533a6703e
nandashivareddygannapureddy-ops <nandashivareddygannapureddy-ops@users.noreply.github.com>
2026-05-17 11:42:27 +0530

    Fixed Render PORT issue

--- server.js
+++ server.js
@@ -105,9 +105,11 @@ const connectDB = async () => {
 
     console.log("DB connection success");
 
-    app.listen(process.env.PORT || 4000, () => {
-      console.log(`Server started on port ${process.env.PORT || 4000}`);
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
+
 
   } catch (err) {
 
